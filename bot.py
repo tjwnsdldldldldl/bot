e5a69b15f2c2d3a572f6ece3b8417d20614a16b9
tjwnsdldldldldl <56643510+tjwnsdldldldldl@users.noreply.github.com>
2019-10-17 22:48:19 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ async def on_ready():
     print("로그인중")
     print("준비됨")
     game = discord.Game("명령어 준비됨")
-    await client.change_presence(status=discord.Status.online, activity=discord.Game(name="{} 명의 유저와 {} 개의 서버에서 함께하는중 ". format(len(client.guilds), len(client.users))))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(name="{} 개의 도시와 {} 명의 주민이 함께하는중 ". format(len(client.guilds), len(client.users))))
     
 
 
