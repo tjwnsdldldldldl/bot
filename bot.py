35b2ee33daa9c090759509087a0015c9a99f0226
tjwnsdldldldldl <56643510+tjwnsdldldldldl@users.noreply.github.com>
2019-10-17 22:54:17 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,13 +18,15 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith("=서준아"):
-        await message.channel.send("?.뭐.왓")
+        await message.channel.send("뭐")
+        await message.channel.send("왓")
+        await message.channel.send("?")
     if message.content.startswith("=심심해"):
         await message.channel.send("배그하던가 아무게임하던가.노래들으러가던가.공부하러가")
     if message.content.startswith("=몇살이니?"):
-        await message.channel.send("관셈을보3")
+        await message.channel.send("관셈을보살")
     if message.content.startswith("=욕잘함?"):
-        await message.channel.send("어 나 욕 잘해")
+        await message.channel.send("너보다는 잘할듯 ^^")
     if message.content.startswith("=도움"):
         await message.channel.send("명령어들 입니다")
         await message.channel.send("#=서준아")
@@ -35,13 +37,15 @@ async def on_message(message):
         await message.channel.send("#=ㅂㅂ")
         await message.channel.send("#=어디살아?")
     if message.content.startswith("=ㅎㅇ"):
-        await message.channel.send("안녕 나는 서준이노예야")
+        await message.channel.send("안녕 나는 장애서준이노예야")
     if message.content.startswith("=어디살아"):
-        await message.channel.send("이세상 어딘가")
+        await message.channel.send("니마음속에")
     if message.content.startswith("=ㅂㅂ"):
         await message.channel.send("잘가 다음에보장!!")
+   
+       await message.channel.send("그래 다음에보장!!!")
     if message.content.startswith("=명령어"):
-        await message.channel.send("알빠?알파?")
+        await message.channel.send("=도움쳐봐 ;;")
 
 
 
